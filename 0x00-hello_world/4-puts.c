7de7cbed8d91a79493f6b9d24d8b7377aa954849
aissami <aissami.said@gmail.com>
2023-10-05 04:29:19 -0700

    hi-c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-	/*
-	 * the main function print \"Programming is like building a multilingual puzzle
-	 * Return: 0 if exited properly, non-zero otherwise
-	 */
+/*
+ * the main function print \"Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
