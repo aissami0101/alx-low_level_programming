7a74db69e3c5c36431d0dcb50d4f12933fe6a2c7
aissami <aissami.said@gmail.com>
2023-10-05 05:01:35 -0700

    hi

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int	main(void)
 	printf("Size of char: %d byte(s)\n", sizeof(char));
 	printf("Size of char: %d byte(s)\n", sizeof(long int));
 	printf("Size of char: %d byte(s)\n", sizeof(long long int));
-
+	printf("Size of char: %d byte(s)\n", sizeof(float));
 	return (0);
 }
