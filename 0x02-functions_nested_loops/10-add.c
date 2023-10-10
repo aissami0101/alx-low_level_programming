6e82c4b559fceba16295be99b022e38c0eb31493
aissami <aissami.said@gmail.com>
2023-10-10 11:43:19 -0700

    hi

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - Adds two integers.
- * @num1: The first integer to be added.
- * @num2: The second integer to be added.
+ * @n: The first integer to be added.
+ * @m: The second integer to be added.
  *
  * Return: The result of the addition.
  */
