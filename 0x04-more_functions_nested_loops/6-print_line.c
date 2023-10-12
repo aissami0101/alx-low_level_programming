c440bbf158b4b69e5687662c19dba630745d4ff6
aissami <aissami.said@gmail.com>
2023-10-12 10:56:41 -0700

    hi

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,9 +8,16 @@ void print_line(int n)
 {
 	int x;
 
-	for (x = 0; x <= n; x++)
+	if (n == 0)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
