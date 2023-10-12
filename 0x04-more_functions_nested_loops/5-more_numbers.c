9f1dc2df90fc024b18b810eda1a97ad2e0260da7
aissami <aissami.said@gmail.com>
2023-10-12 10:44:24 -0700

    hi

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,11 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			_putchar(n + '0');
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
 	}
