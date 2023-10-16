a47c0e9a6a6b5573bab4cbde79291c30a12b3bf8
aissami <aissami.said@gmail.com>
2023-10-16 04:15:36 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "main.h"
+
+void _putchar(char c)
+{
+	write(1, &c1 1);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_C
+#define MAIN_C
+
+void _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
